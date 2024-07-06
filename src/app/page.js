97b4145ac313aa1db6c9a09513b0b5eeaeeeb8e9
Jc sarmiento <jacinto011200@gmail.com>
2024-07-06 22:30:49 +0800

    fix the pages todo not conflict into the sidebar

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,7 @@ export default function Home() {
   return (
     <>
       <Sidebar />
-      <div className='ml-64 p-5'>
-        <div className='text-blue-500'>
-            This is the main content area.
-        </div>
-      </div>
+      
     </>
   );
 }

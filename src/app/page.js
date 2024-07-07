ad6f571839ca481aa1cc5b1e57df5adab777f29f
Jc sarmiento <jacinto011200@gmail.com>
2024-07-07 10:59:32 +0800

    add quickly direct page for testing of the local routes

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,15 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 export default function Home() {
   return (
     <>
-      <div>
-      
-      Hello word
+      <div className="flex justify-center items-center h-screen">
+        <a 
+          href="/dashboard" 
+          className="text-center text-3xl bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Hello world
+        </a>
       </div>
+
       
     </>
   );

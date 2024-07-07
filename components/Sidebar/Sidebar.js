11ca81de5a666002b050486b6db23d63053d439c
Jc sarmiento <jacinto011200@gmail.com>
2024-07-07 11:42:23 +0800

    add dropdown menu item of the sidebar

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -1,9 +1,9 @@
 "use client"
 
-import Image from 'next/image';
 import React, { useState } from 'react';
+import Image from 'next/image';
 import { usePathname } from "next/navigation";
-import { MdOutlineKeyboardDoubleArrowLeft, MdDashboard, MdOutlineSubscriptions } from "react-icons/md";
+import { MdOutlineKeyboardDoubleArrowLeft, MdDashboard, MdOutlineSubscriptions, MdExpandMore, MdExpandLess } from "react-icons/md";
 import { FaSearch, FaUserFriends, FaGlobe, FaRegQuestionCircle } from "react-icons/fa";
 import { GrUserAdmin } from "react-icons/gr";
 import { TfiAnnouncement } from "react-icons/tfi";
@@ -11,64 +11,80 @@ import { RiTeamLine } from "react-icons/ri";
 
 const Sidebar = () => {
     const [open, setOpen] = useState(true);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
     const pathname = usePathname();
 
-    const toggleSearch = () => {
+    const toggleSidebar = () => {
         setOpen(!open);
+        if (open) {
+            setDropdownOpen(false);
+        }
+    };
+
+    const handleDropdownClick = () => {
+        if (!open) {
+            setOpen(true);
+        }
+        setDropdownOpen(!dropdownOpen);
     };
 
     const Menu = [
         {
-            icon: <MdDashboard size={20} />, // Adjust size as needed
-            href: "/dashboard",
+            icon: <MdDashboard size={20} />,
+            href: "/",
             label: "Dashboard",
             roles: [""],
             isActive: pathname === `/`,
         },
         {
-            icon: <GrUserAdmin size={20} />, // Adjust size as needed
+            icon: <GrUserAdmin size={20} />,
             href: "/admin",
             label: "Admin Management",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/admin") : false,
         },
         {
-            icon: <FaUserFriends size={20} />, // Adjust size as needed
+            icon: <FaUserFriends size={20} />,
             href: "/client",
             label: "Client Management",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/client") : false,
         },
         {
-            icon: <FaGlobe size={20} />, // Adjust size as needed
+            icon: <FaGlobe size={20} />,
             href: "/site",
             label: "Site Management",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/site") : false,
         },
         {
-            icon: <FaRegQuestionCircle size={20} />, // Adjust size as needed
-            href: "/request",
+            icon: <FaRegQuestionCircle size={20} />,
+            href: "#",
             label: "Site Request",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/request") : false,
+            submenu: [
+                { href: "/request/option1", label: "Option 1" },
+                { href: "/request/option2", label: "Option 2" },
+                { href: "/request/option3", label: "Option 3" },
+            ],
         },
         {
-            icon: <MdOutlineSubscriptions size={20} />, // Adjust size as needed
+            icon: <MdOutlineSubscriptions size={20} />,
             href: "/subscription",
             label: "Subscription",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/subscription") : false,
         },
         {
-            icon: <TfiAnnouncement size={20} />, // Adjust size as needed
+            icon: <TfiAnnouncement size={20} />,
             href: "/announcement",
             label: "Announcement",
             roles: [""],
             isActive: pathname ? pathname.startsWith("/announcement") : false,
         },
         {
-            icon: <RiTeamLine size={20} />, // Adjust size as needed
+            icon: <RiTeamLine size={20} />,
             href: "/developer",
             label: "Developer",
             roles: [""],
@@ -81,7 +97,7 @@ const Sidebar = () => {
             <div className={`bg-sky-500 h-screen fixed top-0 transition-width duration-300 ${open ? 'w-64' : 'w-24'} md:${open ? 'w-64' : 'w-20'} relative text-white p-5 pt-10`}>
                 <MdOutlineKeyboardDoubleArrowLeft
                     className={`mt-4 absolute border-2 border-gray-700 text-3xl bg-white text-black rounded-full cursor-pointer transition-transform duration-300 ${open ? '-right-3 rotate-0' : '-right-3 rotate-180'}`}
-                    onClick={() => setOpen(!open)}
+                    onClick={toggleSidebar}
                 />
                 <div className='cursor-pointer flex items-center'>
                     <Image 
@@ -92,7 +108,7 @@ const Sidebar = () => {
                         className={`block float-left mr-2 duration-500 ${open ? 'rotate-0 h-auto w-auto' : 'rotate-180 h-[60px] w-[200px]'}`}
                     />
                     <div className='flex justify-center items-center text-center'>
-                        <h1 className={`text-md origin-left font-semibold transition-transform duration-300 ${!open ? 'scale-0' : 'scale-100'}`}>
+                        <h1 className={`text-lg origin-left font-semibold transition-transform duration-300 ${!open ? 'scale-0' : 'scale-100'}`}>
                             Food Store
                         </h1>
                     </div>
@@ -100,7 +116,6 @@ const Sidebar = () => {
                 <div className={`flex items-center rounded-md border border-gray-300 mt-6 py-2 ${open ? 'px-2.5' : 'px-4'}`}>
                     <FaSearch
                         className={`text-white text-2xl block float-left cursor-pointer ${open && 'mr-2'}`}
-                        onClick={toggleSearch} // Click handler to toggle the search bar visibility
                     />
                     <input 
                         type="search" 
@@ -110,14 +125,34 @@ const Sidebar = () => {
                 </div>
                 <ul>
                     {Menu.map((menu, index) => (
-                        <li key={index} className={`text-sm my-2 ${open ? 'flex justify-left items-center' : 'flex justify-center items-center'}`}>
+                        <li key={index} className='text-sm my-2'>
                             <a
                                 href={menu.href}
                                 className={`flex items-center text-white rounded-md p-2 hover:bg-sky-700 ${menu.isActive ? 'bg-sky-700' : ''}`}
+                                onClick={menu.submenu ? handleDropdownClick : undefined}
                             >
                                 {menu.icon}
                                 <span className={`ml-3 ${open ? 'block' : 'hidden'}`}>{menu.label}</span>
+                                {menu.submenu && (
+                                    <span className={`ml-auto transition-transform duration-400 ${dropdownOpen ? 'rotate-80' : ''}`}>
+                                        {dropdownOpen ? <MdExpandLess size={25} /> : <MdExpandMore size={25} />}
+                                    </span>
+                                )}
                             </a>
+                            {menu.submenu && dropdownOpen && open && (
+                                <ul className="ml-6 mt-2">
+                                    {menu.submenu.map((submenu, subIndex) => (
+                                        <li key={subIndex} className="text-sm my-1">
+                                            <a
+                                                href={submenu.href}
+                                                className="flex items-center text-white rounded-md p-2 hover:bg-sky-700"
+                                            >
+                                                <span>{submenu.label}</span>
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
                         </li>
                     ))}
                 </ul>
